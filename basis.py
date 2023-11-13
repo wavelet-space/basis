c068fe296ee6f832e2e0be386b865b1935580331
David Landa <david.landa@proton.me>
2023-11-13 18:26:24 +0100

    Formating

--- basis.py
+++ basis.py
@@ -1,7 +1,7 @@
 def flatten(json: str | dict):
     """
     Flatten a nested structure such as JSON or dictionary.
-    
+
     .. seealso:: :func:`expand`
 
     >>> data = [{
@@ -14,18 +14,20 @@ def flatten(json: str | dict):
     ...             'b': 2,
     ...         }
     ...     }
-    ... ]}   
+    ... ]}
     >>> flatten(data)
-    aa_0, a_1, a_2, b_a, b_b, b_c_a, b_c_b,  
+    aa_0, a_1, a_2, b_a, b_b, b_c_a, b_c_b,
     """
+
 
 def expand():
     """
     Expand a flattened structure.
-  
+
     """
 
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="basis",
-    version="0.1.0",
-    py_modules=["basis"]
-)
+setup(name="basis", version="0.1.0", py_modules=["basis"])
