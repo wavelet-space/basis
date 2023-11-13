2290a41b750be15ce06bd01404381de6509184a6
David Landa <david.landa@proton.me>
2023-11-13 17:17:31 +0100

    Update main module

--- basis.py
+++ basis.py
@@ -1,22 +1,10 @@
-
-
-
-def nested():
-    """
-    Make flat structure nested again. 
-    """
-
 def flatten(json: str | dict):
     """
     Flatten a nested structure such as JSON or dictionary.
     
-    >>> data = {
-    ...     'a': [1, 2, 3]
-    ... } 
-    
-    expect a_0 a_1 a_2
+    .. seealso:: :func:`expand`
 
-    >>> data = {
+    >>> data = [{
     ...     'a': [1, 2, 3],
     ...     'b': {
     ...         'a': 1,
@@ -26,9 +14,15 @@ def flatten(json: str | dict):
     ...             'b': 2,
     ...         }
     ...     }
-    ... }   
-    
-    expected flatten a_0, a_1, a_2, b_a, b_b, b_c_a,, b_c_b,  
+    ... ]}   
+    >>> flatten(data)
+    aa_0, a_1, a_2, b_a, b_b, b_c_a, b_c_b,  
+    """
+
+def expand():
+    """
+    Expand a flattened structure.
+  
     """
 
 
