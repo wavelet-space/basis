1b2fdbb3a9b450111267004b265564bc97feea08
David Landa <david.landa@proton.me>
2023-11-14 09:50:28 +0100

    Formating

--- basis.py
+++ basis.py
@@ -25,7 +25,7 @@ def flatten(json: str | dict, separator="."):
 def expand(data, separator="."):
     """
     Expand a flattened structure.
-    
+
     >>> data = [{'a[0]': 1, 'a[1]': 2, 'a[2]': 3, 'b_a': 1, 'b_b': 2, 'b_c_a': 1, 'b_c_b': 2}]
     >>> expand(data, separator="_")
     [{'a': [1, 2, 3], 'b': {'a': 1, 'b': 2, 'c': {'a': 1, 'b': 2}}]}
