0acd2d851542e4f35e910dc86d09515c4e6883e4
David Landa <david.landa@proton.me>
2024-03-01 10:47:29 +0100

    Fixes

--- src/basis/persistence/interface.py
+++ src/basis/persistence/interface.py
@@ -2,8 +2,48 @@
 Abstractions around PEP 249 <https://peps.python.org/pep-0249/>_
 """
 
-from basis.persistence.interface import (
-    Cursor as Cursor,
-    CursorLike as CursorLike,
-    Connection as Connection,
-)
+
+from typing import Protocol
+
+
+class Cursor(Protocol):
+    # REQUIRED
+
+    @property
+    def description(self): ...
+
+    @property
+    def rowcount(self) -> int: ...
+
+    # execute()
+    # execuremany()
+    # fetchone()
+    # fetchmany()
+    # fetchall()
+    # nextset()
+    # arraysize()
+    # setinputsize()
+    # setoutputsize()
+
+
+class CursorExtended(Cursor):
+    # OPTIONAL
+    ...
+
+
+class Connection(Protocol):
+    """Represents a database connection."""
+
+    autocommit: bool
+
+    def close(self) -> None:
+        ...
+
+    def commit(self) -> None:
+        ...
+
+    def rollback(self) -> None:
+        ...
+
+    def cursor(self) -> Cursor:
+        ...
