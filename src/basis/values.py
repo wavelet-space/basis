a9b03b3143732f16a42fed2cec81d3843c3d0e57
David Landa <david.landa@protonmail.com>
2024-03-10 11:01:15 +0100

    Update values.py

--- src/basis/values.py
+++ src/basis/values.py
@@ -6,4 +6,20 @@ __all__ = ["SingleCaseValue"]
 
 @dataclass(frozen=True, slots=True)
 class SingleCaseValue[T]:
+    """
+    Represents a data class with a single value.
+    
+    This is a convention, so you don't have to think about how to name a value attribute.
+    
+    Example:
+
+    .. code-block:: python
+
+        @dataclass(frozen=True, slots=True)
+        class Name(SingleCaseValue):
+            def __post_init__(self) -> None:
+                if len(self.value) == 0:
+                    raise ValueError("empty name is not allowed")
+
+    """
     value: T
